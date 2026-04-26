e1707c583109fb5635bd04ea080cd0bf9637ac86
violet <83107846+violetEvergarden0@users.noreply.github.com>
2026-04-26 13:50:26 +0800

    modify SUMMARIZER_MEM_FRACTION_STATIC

--- inference/serve_summarizer_qwen32b_2gpu.sh
+++ inference/serve_summarizer_qwen32b_2gpu.sh
@@ -11,4 +11,4 @@ CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-1,2}" python3 -m sglang.launch_ser
   --tp-size "${SUMMARIZER_TP_SIZE:-2}" \
   --dtype "${SUMMARIZER_DTYPE:-bfloat16}" \
   --host "${SUMMARIZER_HOST:-0.0.0.0}" --port "${SUMMARIZER_PORT:-8181}" \
-  --mem-fraction-static "${SUMMARIZER_MEM_FRACTION_STATIC:-0.3}"
+  --mem-fraction-static "${SUMMARIZER_MEM_FRACTION_STATIC:-0.75}"
